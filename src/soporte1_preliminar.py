4ef71504f166eeab278dd1bdde3aa1cab0dd61f7
Lorena López Arenas <lo.lopezarenas@gmail.com>
2026-03-23 14:12:41 +0100

    Funciones

---
+++ src/soporte1_preliminar.py
@@ -0,0 +1,59 @@
+# Tratamiento de datos
+import pandas as pd 
+from IPython.display import display
+
+# ------------------------------FUNCIONES------------------------------
+
+# -------------Función eda_preliminar-------------
+def eda_preliminar(df):
+    """Realiza un análisis exploratorio preliminar sobre un DataFrame dado.
+
+       Este análisis incluye:
+       - Muestra de las 3 primeras y últimas filas.
+       - Muestra de 3 filas aleatorias.
+       - Dimensión del DataFrame.
+       - Información general del DataFrame (nulos, tipos de datos, etc.).
+       - Valores nulos por columna (tanto en términos absolutos como relativos).
+       - Conteo de valores duplicados.
+       - Listado del nombre de las columnas.
+       - Estadísticas descriptivas de las variables numéricas.
+       - Estadísticas descriptivas de las variables categóricas.
+
+    Args:
+        df: DataFrame a analizar
+    """
+
+    print('\n\033[1m👀 Visualización de las primeras y últimas filas\033[0m\n')
+    display(df.head(3))
+    display(df.tail(3))
+
+    print('\n\033[1m🎲 Visualización de filas aleatorias\033[0m\n')
+    display(df.sample(3))
+
+    print('\n\n\033[1m📏 Dimensión del DataFrame\033[0m\n')
+    print(f'Número de filas del DataFrame: {df.shape[0]}')
+    print(f'Número de columnas del DataFrame: {df.shape[1]}')
+    
+    print('\n\n\033[1m🧾 Información general\033[0m\n')
+    df.info()
+
+    print('\n\n\033[1m❌ Detección de nulos\033[0m\n')
+    nulos = df.isnull().sum()
+    print(f"El número de valores nulos de cada columna es: \n{nulos[nulos > 0]}\n")
+
+    nulos_porcentaje = df.isnull().sum() / (df.shape[0])*100
+    print(f"El porcentaje de valores nulos de cada columna es: \n{nulos_porcentaje[nulos_porcentaje > 0].round(2)}")
+
+    print('\n\n\033[1m🔁 Detección de duplicados\033[0m\n')
+    print(f'El número de duplicados del dataset es: {df.duplicated().sum()}')
+
+    print('\n\n\033[1m🏷️ Nombres de las columnas\033[0m\n')
+    display(df.columns)
+
+    print('\n\n\033[1m🔢 Estadísticas descriptivas de las variables numéricas\033[0m\n')
+    display(df.describe().round(2).T)
+
+    print('\n\n\033[1m🔡 Estadísticas descriptivas de las variables categóricas\033[0m\n')
+    display(df.describe(include=['category', 'str']).T)
+
+#------------------------------------------------------------------------------------------
---
+++ src/soporte2_limpieza.py
@@ -0,0 +1,101 @@
+# Tratamiento de datos
+import pandas as pd 
+from IPython.display import display
+
+# -------------Función para estandarizar nombres de columnas-------------
+def estandar_columns(df):
+    """Función que estandariza los nombres de las columnas de un DataFrame dado:
+       - Cambio a minúsculas
+       - Eliminación de espacios al principio y al final
+       - Cambio de espacios intermedios por "_"
+
+    Args:
+        df: DataFrame
+    """
+    df.columns = df.columns.str.lower().str.strip().str.replace(" ", "_")
+
+#------------------------------------------------------------------------------------------
+
+# -------------Función para eliminar columnas-------------
+def eliminar_columns(df, cols):
+    """Función que elimina columnas específicas de un DataFrame dado.
+
+    Args:
+        df: DataFrame
+        cols: columnas a eliminar
+    """
+    
+    return df.drop(columns=cols)
+
+#------------------------------------------------------------------------------------------
+
+# -------------Función para pasar datos a minúscula-------------
+def minus (df, cols):
+    """Función que transforma los datos de columnas dadas de una DataFrame a minúsculas.
+
+    Args:
+        df: DataFrame
+        cols: columnas a modificar
+    """
+    for col in cols:
+        df[col] = df[col].str.lower().str.strip()
+
+#------------------------------------------------------------------------------------------
+
+# -------------Función para pasar sacar los datos únicos de columnas-------------
+def datos_unicos(df, cols):
+    for col in cols:
+        print(f'Los datos únicos de la varible {col} son:\n {df[col].unique()}\n')
+        print('-----------------------------------------------------------------')
+
+#------------------------------------------------------------------------------------------
+
+# -------------Función para reemplazar palabras o caracteres de columnas-------------
+def reemplazar (df, cols, x, y):
+    """Función que sirve para reemplazar palabras o caracteres de columnas dadas de un DataFrame.
+
+    Args:
+        df: DataFrame
+        cols: columnas en las cuales queremos reemplazar datos
+        x: dato a reemplazar
+        y: dato por el que reemplazar
+    """
+    for col in cols:
+        df[col] = df[col].str.replace(x,y)
+
+#------------------------------------------------------------------------------------------
+
+def rellenar_nulos_cat(df, cols, x):
+    for col in cols:
+       df[col] = df[col].fillna(x) 
+
+def rellenar_nulos_num(df, cols):
+    for col in cols:
+       df[col] = df[col].fillna(df[col].mean()) 
+
+def rellenar_nulos_moda(df, cols):
+    for col in cols:
+        df[col] = df[col].fillna(df[col].mode()[0])
+
+
+
+# -------------Función para un análisis rápido-------------
+def analisis_rapido (df, n=3):
+    """Función que genera un análisis rápido del DataFrame.
+
+    Args:
+        df: Dataframe
+        n: número de filas (por defecto = 3)
+    """
+    print(f'\n\nLas {n} primeras filas del Dataframe son:\n')
+    display(df.head(n))
+    print('-----------------------------------------------------------')
+
+    print(f'\nLa información básica del Dataframe es la siguiente:\n')
+    df.info()
+    print('-----------------------------------------------------------')
+
+    print(f'\nEl número de nulos por columna del Dataframe es:\n')
+    display(df.isnull().sum())
+
+#------------------------------------------------------------------------------------------
